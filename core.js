95e06616f7c1bc137e6c8bc8d4690073d7879fc6
Kevin Alwell <alwellkevin44@gmail.com>
2018-03-09 09:12:13 -0500

    chat window

--- core.js
+++ core.js
@@ -1,5 +1,19 @@
-$( document ).ready(function() {
-    // DOM ready
+$(document).ready(function () {
 
- 
+    var activeUser = localStorage.getItem('active_user');
+    if (activeUser) {
+        $("#signInContainer").hide();
+
+    }
+
+    $("#join_discussion").click(function () {
+        activeUser = $("#inputEmail").val();
+        localStorage.setItem('active_user', activeUser);
+    })
+
+    $("#profile-img").click(function () {
+        localStorage.removeItem('active_user');
+        activeUser = null;
+        $("#signInContainer").show();
+    })
 });
