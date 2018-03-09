84010b058a4e1911e84b579908347ac4a420f599
Kevin Alwell <alwellkevin44@gmail.com>
2018-03-09 09:35:28 -0500

    log in log out

--- core.js
+++ core.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
 
     var activeUser = localStorage.getItem('active_user');
-    if (activeUser) {
-        $("#signInContainer").hide();
-
+    if (activeUser && !onChatScreen()) {
+        enterChatScreen();
+    } else if (!activeUser && onChatScreen()) {
+        goToLogin();
     }
 
-    $("#join_discussion").click(function () {
+    $(".form-signin").submit(function () {
         activeUser = $("#inputEmail").val();
         localStorage.setItem('active_user', activeUser);
+        enterChatScreen();
     })
 
     $("#profile-img").click(function () {
-        localStorage.removeItem('active_user');
-        activeUser = null;
-        $("#signInContainer").show();
+        goToLogin();
     })
 });
+
+function enterChatScreen() {
+    $("#signInContainer").hide();
+    window.location.href = './chatWindow.html';
+    return false;
+}
+
+function goToLogin() {
+    window.location.href = './index.html';
+    localStorage.removeItem('active_user');
+    activeUser = null;
+    $("#signInContainer").show();
+}
+
+function onChatScreen() {
+    if (window.location.href.indexOf("chatWindow") === -1) {
+        return false
+    }
+    return true
+}
